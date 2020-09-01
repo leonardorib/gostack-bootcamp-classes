acc8350de0d5c5af24179d028096602802c21941
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2020-09-01 10:19:43 -0300

    styled dashboard page

--- level_03/3-1-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ level_03/3-1-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt='Github Explorer' />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action=''>
+        <input type='text' placeholder='Digite o nome do repositório' />
+        <button type='submit'>Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href='teste'>
+          <img
+            src='https://avatars0.githubusercontent.com/u/11333829?s=400&u=5ac09229e43715bb45e81eab1c4f002b133a4452&v=4'
+            alt='Leonardo Ribeiro'
+          />
+          <div>
+            <strong>leonardorib/proffy</strong>
+            <p>Connecting students and teachers.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href='teste'>
+          <img
+            src='https://avatars0.githubusercontent.com/u/11333829?s=400&u=5ac09229e43715bb45e81eab1c4f002b133a4452&v=4'
+            alt='Leonardo Ribeiro'
+          />
+          <div>
+            <strong>leonardorib/proffy</strong>
+            <p>Connecting students and teachers.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href='teste'>
+          <img
+            src='https://avatars0.githubusercontent.com/u/11333829?s=400&u=5ac09229e43715bb45e81eab1c4f002b133a4452&v=4'
+            alt='Leonardo Ribeiro'
+          />
+          <div>
+            <strong>leonardorib/proffy</strong>
+            <p>Connecting students and teachers.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
