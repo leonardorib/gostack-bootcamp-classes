86f6b90a41f667f0d9f07ce1f524c3f199b226ba
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2020-09-05 13:45:14 -0300

    started to use unform

--- level_03/3-2-iniciando-frontend-web-gobarber/src/pages/SignUp/index.tsx
+++ level_03/3-2-iniciando-frontend-web-gobarber/src/pages/SignUp/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FiArrowLeft, FiLogIn, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
+import { Form } from '@unform/web';
+
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -8,30 +10,36 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt='GoBarber' />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-      <form>
-        <h1>Faça seu logon</h1>
-        <Input name='name' icon={FiUser} type='text' placeholder='Nome' />
-        <Input name='email' icon={FiMail} type='text' placeholder='E-mail' />
-        <Input
-          name='password'
-          icon={FiLock}
-          type='password'
-          placeholder='Senha'
-        />
-        <Button type='submit'>Cadastrar</Button>
-      </form>
-      <a href='signup'>
-        <FiArrowLeft />
-        Voltar para logon
-      </a>
-    </Content>
-  </Container>
-);
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt='GoBarber' />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+          <Input name='name' icon={FiUser} type='text' placeholder='Nome' />
+          <Input name='email' icon={FiMail} type='text' placeholder='E-mail' />
+          <Input
+            name='password'
+            icon={FiLock}
+            type='password'
+            placeholder='Senha'
+          />
+          <Button type='submit'>Cadastrar</Button>
+        </Form>
+        <a href='signup'>
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
