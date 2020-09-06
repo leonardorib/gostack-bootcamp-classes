5b84e02e3264d3dff0c27a926e40300f3cf6f441
Leonardo <leonardo.rib@hotmail.com>
2020-09-06 18:48:13 -0300

    improved input usability/learned useCallback

--- level_03/3-2-iniciando-frontend-web-gobarber/src/components/Input/styles.ts
+++ level_03/3-2-iniciando-frontend-web-gobarber/src/components/Input/styles.ts
@@ -1,6 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
   border: 2px solid #232129;
@@ -16,6 +21,15 @@ export const Container = styled.div`
   svg {
     margin-right: 16px;
   }
+
+  ${props => props.isFocused && css`
+    color: #ff9000;
+    border-color: #ff9000;
+  `}
+
+  ${props => props.isFilled && css`
+    color: #ff9000;
+  `}
 
   input {
     flex: 1;
