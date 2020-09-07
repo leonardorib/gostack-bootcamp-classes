e280085b50e4e846bfaad5390813d0dfb658e0f8
Leonardo <leonardo.rib@hotmail.com>
2020-09-07 15:27:39 -0300

    created private routes

--- level_03/3-2-iniciando-frontend-web-gobarber/src/routes/index.tsx
+++ level_03/3-2-iniciando-frontend-web-gobarber/src/routes/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path='/' exact component={SignIn}></Route>
       <Route path='/signup' exact component={SignUp}></Route>
+      <Route
+        path='/dashboard'
+        exact
+        component={Dashboard}
+        isPrivate={true}
+      ></Route>
     </Switch>
   );
 };
