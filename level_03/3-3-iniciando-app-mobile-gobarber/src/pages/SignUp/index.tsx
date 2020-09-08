b0f7996cf71dc2585959facf93074fb4717f97a9
Leonardo <leonardo.rib@hotmail.com>
2020-09-08 11:07:19 -0300

    integrated @unform lib

--- level_03/3-3-iniciando-app-mobile-gobarber/src/pages/SignUp/index.tsx
+++ level_03/3-3-iniciando-app-mobile-gobarber/src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   Image,
   View,
@@ -8,6 +8,9 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -22,6 +25,7 @@ import {
 } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
 
   return (
@@ -40,12 +44,25 @@ const SignUp: React.FC = () => {
             <View>
               <Title>Crie sua conta</Title>
             </View>
+            <Form
+              ref={formRef}
+              onSubmit={(data) => {
+                console.log(data);
+              }}
+              style={{ width: '100%' }}
+            >
+              <Input name='name' icon='user' placeholder='Nome' />
+              <Input name='email' icon='mail' placeholder='E-mail' />
+              <Input name='password' icon='lock' placeholder='Senha' />
 
-            <Input name='name' icon='user' placeholder='Nome' />
-            <Input name='email' icon='mail' placeholder='E-mail' />
-            <Input name='password' icon='lock' placeholder='Senha' />
-
-            <Button onPress={() => {}}>Cadastrar</Button>
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Cadastrar
+              </Button>
+            </Form>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
