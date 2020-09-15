080f0ae853a5de55b8c9485835750800dbb900e2
Leonardo <leonardo.rib@hotmail.com>
2020-09-14 23:29:08 -0300

    reorganized api created in level 2 into a more scalable folder strucuture, following domain driven design

---
+++ level_04/4-1-arquitetura-e-testes-node/src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,42 @@
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '@shared/errors/AppError';
+
+import { startOfHour } from 'date-fns';
+
+interface CreateAppointmentDTO {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({
+    date,
+    provider_id,
+  }: CreateAppointmentDTO): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
