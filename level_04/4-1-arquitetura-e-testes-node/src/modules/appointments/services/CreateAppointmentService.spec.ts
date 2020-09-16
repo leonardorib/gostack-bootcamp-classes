ff530fe165c1c9853cbbbfe61f39dad360e27630
Leonardo <leonardo.rib@hotmail.com>
2020-09-15 21:21:15 -0300

    created first test - CreateAppointmentService

--- level_04/4-1-arquitetura-e-testes-node/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ level_04/4-1-arquitetura-e-testes-node/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,23 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('shoulb be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12121212',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12121212');
+  });
+
+  // it('should be not able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
