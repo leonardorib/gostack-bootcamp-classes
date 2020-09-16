a20e67aedb562976c6b3676fa5543bb86596dfa8
Leonardo <leonardo.rib@hotmail.com>
2020-09-16 16:23:46 -0300

    created tests for authentication with non existing email and incorrect password

--- level_04/4-1-arquitetura-e-testes-node/src/modules/users/services/AuthenticateUserService.spec.ts
+++ level_04/4-1-arquitetura-e-testes-node/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -35,4 +35,49 @@ describe('AuthenticateUser', () => {
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(userWithoutPassword);
   });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: 'wrongPassword',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
