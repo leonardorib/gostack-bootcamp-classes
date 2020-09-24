32368a2c3e0ef46080428bcb1c918ed2d322a544
Leonardo <leonardo.rib@hotmail.com>
2020-09-24 20:35:58 -0300

    created MailTemplateProvider with handlebars

--- level_04/4-2-continuando-backend-do-app/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ level_04/4-2-continuando-backend-do-app/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -7,6 +7,7 @@ import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import AppError from '@shared/errors/AppError';
+import { template } from 'handlebars';
 
 interface IRequest {
   email: string;
@@ -42,10 +43,20 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido ${token}`
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
--- level_04/4-2-continuando-backend-do-app/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ level_04/4-2-continuando-backend-do-app/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,11 +1,22 @@
 import nodemailer, { Transporter } from 'nodemailer';
 
+import ISendMailDTO from '../dtos/ISendMailDTO';
 import IMailProvider from '../models/IMailProvider';
 
+import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
+import { inject, injectable } from 'tsyringe';
+
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
+  private mailTemplateProvider: IMailTemplateProvider;
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    mailTemplateProvider: IMailTemplateProvider
+  ) {
+    this.mailTemplateProvider = mailTemplateProvider;
+
     nodemailer.createTestAccount().then((account) => {
       const transporter = nodemailer.createTransport({
         host: account.smtp.host,
@@ -21,12 +32,23 @@ export default class EtherealMailProvider implements IMailProvider {
     });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
     const message = await this.client.sendMail({
-      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
-      to,
-      subject: 'Recuperação de senha',
-      text: body,
+      from: {
+        name: from?.name || 'Equipe Go Barber',
+        address: from?.email || 'equipe@gobarber.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
     });
 
     console.log('Message sent: %s', message.messageId);
