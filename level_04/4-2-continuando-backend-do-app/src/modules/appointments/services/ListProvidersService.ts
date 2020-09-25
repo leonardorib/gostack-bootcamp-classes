10f08f3724df9de35e5ccff60a8f1cb4e2151b36
Leonardo <leonardo.rib@hotmail.com>
2020-09-25 09:07:27 -0300

    created ListProvidersService and it's corresopnding tests/router/controller

---
+++ level_04/4-2-continuando-backend-do-app/src/modules/appointments/services/ListProvidersService.ts
@@ -0,0 +1,30 @@
+import User from '@modules/users/infra/typeorm/entities/User';
+import { injectable, inject } from 'tsyringe';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ListProvidersService {
+  private usersRepository: IUsersRepository;
+
+  constructor(
+    @inject('UsersRepository')
+    usersRepository: IUsersRepository
+  ) {
+    this.usersRepository = usersRepository;
+  }
+
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    const users = await this.usersRepository.findAllProviders({
+      except_user_id: user_id,
+    });
+
+    return users;
+  }
+}
+
+export default ListProvidersService;
