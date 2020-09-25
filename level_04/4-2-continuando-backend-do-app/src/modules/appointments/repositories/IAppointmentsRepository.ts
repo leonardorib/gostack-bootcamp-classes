557a4205410df6fad175d7074f10dc21cc42f193
Leonardo <leonardo.rib@hotmail.com>
2020-09-25 11:49:37 -0300

    Created service and it's test to list hours available in a day (ListProviderDayAvailabilityService)

--- level_04/4-2-continuando-backend-do-app/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ level_04/4-2-continuando-backend-do-app/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,7 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
@@ -9,5 +10,9 @@ export default interface IAppointmentRepository {
 
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProviderDTO
+  ): Promise<Appointment[]>;
+
+  findAllInDayFromProvider(
+    data: IFindAllInDayFromProviderDTO
   ): Promise<Appointment[]>;
 }
