2262840129c24ae7102f98d14abf75dcbaae5cac
Leonardo <leonardo.rib@hotmail.com>
2020-09-30 22:39:26 -0300

    Integrated with AWS S3 to storage in production

--- level_04/4-2-continuando-backend-do-app/src/modules/users/infra/http/routes/users.routes.ts
+++ level_04/4-2-continuando-backend-do-app/src/modules/users/infra/http/routes/users.routes.ts
@@ -8,7 +8,7 @@ import UserAvatarController from '../controllers/UserAvatarController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
