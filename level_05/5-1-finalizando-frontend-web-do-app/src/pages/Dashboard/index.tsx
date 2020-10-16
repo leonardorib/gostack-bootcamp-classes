3828bfdc7ce52008371b3119ab096a1008c5b7fa
Leonardo <leonardo.rib@hotmail.com>
2020-10-16 11:46:40 -0300

    Added 'Next Appointment' to dashboard page

--- level_05/5-1-finalizando-frontend-web-do-app/src/pages/Dashboard/index.tsx
+++ level_05/5-1-finalizando-frontend-web-do-app/src/pages/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
@@ -28,6 +37,33 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src='http://localhost:3333/files/3e67e5c38dcb9976a50c-avatar1.jpg'
+                alt='Leonardo Ribeiro'
+              />
+              <strong>Leonardo Ribeiro</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
